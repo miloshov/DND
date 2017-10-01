ac24761685ad43fc62bf8725e6b63a3072becb83
miloshov <miloshov84@gmail.com>
2017-10-01 23:32:22 +0200

    Minor change

---
+++ DND/Controller/AboutUsVC.swift
@@ -0,0 +1,34 @@
+//
+//  AboutUsVC.swift
+//  DND
+//
+//  Created by Milos Hovjecki on 10/1/17.
+//  Copyright © 2017 Milos Hovjecki. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsVC: UIViewController {
+    
+    // MARK: Properties
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func CallUsBtnPressed(_ sender: UIButton) {
+        
+        if #available(iOS 10.0, *) {
+            callNumber(phoneNumber: "+381113196706")
+        } else {
+            // Fallback on earlier versions
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
