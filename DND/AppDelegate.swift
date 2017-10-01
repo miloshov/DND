cf22038600e423357f2613d996170887d0fc341e
miloshov <miloshov84@gmail.com>
2017-10-01 15:23:27 +0200

    Changes made in AppDelegate file: - imported all necessary pods - Notification configured - Badge was left out on purpose
    All Certificates are configured on Apple account.
    Firebase account was configured also with name "DND for iOS" in "DND Commerce" project.Hajora account was used.
    GoogleService-info.plist was implemented in project.
    Push Notifications are tested and working.
    
    No other changes were made at this time...

--- DND/AppDelegate.swift
+++ DND/AppDelegate.swift
@@ -7,40 +7,101 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import FirebaseInstanceID
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
+    
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (isGranted, err) in
+            // Setting up thet type of the notifications we want to receive and configuring Firebase App
+            // Handling an errors
+            
+            if err != nil {
+                
+                // Something bad happened
+                
+            } else {
+                
+                UNUserNotificationCenter.current().delegate = self
+                Messaging.messaging().delegate = self
+                
+            }
+            
+        }
+        
+        application.registerForRemoteNotifications()
+        FirebaseApp.configure()
+        
+        Thread.sleep(forTimeInterval: 2) // Launch screen manual running time
+        
         return true
     }
-
+    
+    func ConnectToFCM() {
+        // Function for connectiong with Firebase Connection manager.
+        // It should be called when application became active and it should be uncalled when application enters background
+        // It should be called when token is refreshed
+        
+        Messaging.messaging().shouldEstablishDirectChannel = true
+        
+        if let token = InstanceID.instanceID().token() {
+            print("DCS: " + token)
+        }
+        
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        Messaging.messaging().shouldEstablishDirectChannel = false
+        
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        ConnectToFCM()
+        
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
     }
-
-
+    
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        
+        ConnectToFCM()
+        
+    }
+    
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound])
+        
+        UIApplication.shared.applicationIconBadgeNumber += 1
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "com.DouglasDevlops.BadgeWasUpdated"), object: nil)
+        
+    }
+    
 }
 
