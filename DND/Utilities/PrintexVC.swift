c643dc0a1cf2676f226b7c621fa49a6b4269ba01
miloshov <miloshov84@gmail.com>
2017-10-07 16:36:40 +0200

    All PatnerVC's are implemented. Images for them are missing, because DND deleted them from the server.
    No ther changes made today.

---
+++ DND/Utilities/PrintexVC.swift
@@ -0,0 +1,25 @@
+//
+//  PrintexVC.swift
+//  DND
+//
+//  Created by Milos Hovjecki on 10/7/17.
+//  Copyright © 2017 Milos Hovjecki. All rights reserved.
+//
+
+import UIKit
+
+class PrintexVC: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBAction func backBtnPressed (_sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ DND/Utilities/SteelcoVC.swift
@@ -0,0 +1,25 @@
+//
+//  SteelcoVC.swift
+//  DND
+//
+//  Created by Milos Hovjecki on 10/7/17.
+//  Copyright © 2017 Milos Hovjecki. All rights reserved.
+//
+
+import UIKit
+
+class SteelcoVC: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBAction func backBtnPressed (_sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
